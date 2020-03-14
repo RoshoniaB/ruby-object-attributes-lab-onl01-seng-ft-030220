2a64a7d0a8c4a4250cc3468317cc508420013eb7
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-14 23:29:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
-class dog 
-  def name 
-    @name 
+class Dog
+  def name
+    @name
   end
 
-  def name(name)
-    @name= name
-  end  
+  def name=(name)
+    @name = name
+  end
 
-  def breed 
-    @breed 
-  end 
+  def breed
+    @breed
+  end
 
-  def breed(breed)
-    @breed=breed
+  def breed=(breed)
+    @breed = breed
   end
 end
